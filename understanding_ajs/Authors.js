b42f1e97f2e7c9b6aaa9701d9edaf99952311bea
jcooke1986uk <jcooke.web@gmail.com>
2017-03-04 11:13:06 +0000

    Angular Expressions and Directives
    Intro to simple Expressions and Directives

---
+++ understanding_ajs/Authors.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+var AuthorApp = angular.module('AuthorApp', []);
+AuthorApp.controller("MyController", function ($scope) {
+    $scope.authors =
+    [
+        { 'name': 'Jenny Cooke' },
+        { 'name': 'Charles Darwin' },
+        { 'name': 'Santa Claus' },
+        { 'name': 'Deapool' }
+    ];
+});
